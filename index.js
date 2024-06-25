ad147b971e4a8132f7e641a0f0c6022d48dce2a9
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-06-25 14:44:11 +0100

    modified index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/todolist')
+mongoose.connect('mongodb://0.0.0.0:27017/todolist')
 
 // get request
 app.get('/get', (req, res) => {
